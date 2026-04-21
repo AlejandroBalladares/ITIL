17d488f24aa77f2602970410f31143b396ea8405
AlejandroBalladares <44703962+AlejandroBalladares@users.noreply.github.com>
2026-04-21 19:59:14 -0300

    Delete Frontend directory

--- Frontend/app/reportes/_components/total-view.tsx
+++
@@ -1,8 +0,0 @@
-export function TotalView({ total }: { total: number }) {
-  return (
-    <div className="my-auto text-center">
-      <h3 className="text-xl font-bold">Total</h3>
-      <span className="text-8xl font-bold">{total}</span>
-    </div>
-  );
-}
